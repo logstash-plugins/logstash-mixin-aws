b49bf5c441c79ed1f3d643a07eea67d23c707b28
Chris Abberley <26394346+cabberley@users.noreply.github.com>
2020-07-28 11:04:23 +1000

    typo fxes

--- lib/logstash/plugin_mixins/aws_config/v2.rb
+++ lib/logstash/plugin_mixins/aws_config/v2.rb
@@ -42,18 +42,13 @@ module LogStash::PluginMixins::AwsConfig::V2
                   end
                   if @role_arn && @external_id && @proxy_uri && @access_key_id && @secret_access_key
                    Aws::AssumeRoleCredentials.new(
-                    :client => Aws::STS::Client.new(:access_key_id => @access_key_id,
-                      :secret_access_key => @secret_access_key,
-                      :region => @region,
-                      :http_proxy => @proxy_uri),
+                    :client => Aws::STS::Client.new(:access_key_id => @access_key_id, :secret_access_key => @secret_access_key, :region => @region, :http_proxy => @proxy_uri),
                     :role_arn => @role_arn,
                     :role_session_name => @role_session_name,
                     :external_id => @external_id)
                   elsif @role_arn && @external_id && @access_key_id && @secret_access_key
                    Aws::AssumeRoleCredentials.new(
-                    :client => Aws::STS::Client.new(:access_key_id => @access_key_id,
-                      :secret_access_key => @secret_access_key,
-                      :region => @region),
+                    :client => Aws::STS::Client.new(:access_key_id => @access_key_id, :secret_access_key => @secret_access_key, :region => @region),
                     :role_arn => @role_arn,
                     :role_session_name => @role_session_name,
                     :external_id => @external_id)
@@ -68,14 +63,10 @@ module LogStash::PluginMixins::AwsConfig::V2
                         :role_arn => @role_arn,
                         :role_session_name => @role_session_name)
                     elsif @access_key_id && @secret_access_key
-                      Aws::Credentials.new(:access_key_id => @access_key_id,
-                        :secret_access_key => @secret_access_key,
-                        :session_token => @session_token)
+                      Aws::Credentials.new(:access_key_id => @access_key_id, :secret_access_key => @secret_access_key, :session_token => @session_token)
                     elsif @aws_credentials_file
                       credentials_opts = YAML.load_file(@aws_credentials_file)
-                      Aws::Credentials.new(:access_key_id => @access_key_id,
-                        :secret_access_key => @secret_access_key,
-                        :session_token => @session_token)
+                      Aws::Credentials.new(:access_key_id => @access_key_id, :secret_access_key => @secret_access_key, :session_token => @session_token)
                     end
                 end
   end
